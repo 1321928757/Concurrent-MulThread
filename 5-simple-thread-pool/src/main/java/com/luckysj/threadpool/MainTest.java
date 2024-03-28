8cac3131e38cf760581d94b85bbdeca869ad11c0
刘仕杰 <1321928757@qq.com>
2024-03-28 22:06:06 +0800

    线程工厂实现

--- 5-simple-thread-pool/src/main/java/com/luckysj/threadpool/MainTest.java
+++ 5-simple-thread-pool/src/main/java/com/luckysj/threadpool/MainTest.java
@@ -4,6 +4,8 @@ import com.luckysj.threadpool.core.ThreadPool;
 import com.luckysj.threadpool.core.WorkQueue;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
